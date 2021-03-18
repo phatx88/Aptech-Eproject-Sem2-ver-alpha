8e5e760acea060c7ea39d9361e9e2d2182dd96cd
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2021-03-18 18:42:20 +0700

    commit date 3/18

--- test/routes/web.php
+++ test/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('pages.home');
+});
+
 Route::get('/home', function () {
     return view('pages.home');
 });
