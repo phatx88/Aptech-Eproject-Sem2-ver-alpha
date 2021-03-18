8e0b8ad9a772c90119b424c2118a0ad68147657d
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2021-03-18 18:40:19 +0700

    commit

--- test/routes/web.php
+++ test/routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
+Route::get('/home', function () {
+    return view('pages.home');
+});
+Route::get('/product', function () {
+    return view('pages.product');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/blog', function () {
+    return view('pages.blog');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('/single-product', function () {
+    return view('pages.single_product');
+});
+
+Route::get('/cart', function () {
+    return view('pages.cart');
+});
+
+Route::get('/check-out', function () {
+    return view('pages.checkout');
 });
