69f744cdb804402b279e10480f5ecf5e7ba2d842
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2021-04-07 15:49:25 +0700

    update products phat

--- test/resources/views/pages/product.blade.php
+++ test/resources/views/pages/product.blade.php
@@ -15,6 +15,73 @@
     <section class="ftco-section">
         <div class="container">
             <div class="row">
+                {{-- SIDE BAR  --}}
+                <div class="col-md-3">
+                    <div class="sidebar-box ftco-animate">
+                        <div class="categories">
+                            <h3>Product Types</h3>
+                            <ul class="p-0">
+                                <li><a href="#">All Types <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Brandy <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Gin <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Rum <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Tequila <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Vodka <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Whiskey <span class="fa fa-chevron-right"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="sidebar-box ftco-animate">
+                        <div class="categories">
+                            <h3>Product Featured</h3>
+                            <ul class="p-0">
+                                <li><a href="#">All Featured <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">On Sale <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">Best Seller <span class="fa fa-chevron-right"></span></a></li>
+                                <li><a href="#">latest Arrival <span class="fa fa-chevron-right"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="sidebar-box ftco-animate">
+                        <h3>Recent Blog</h3>
+                        <div class="block-21 mb-4 d-flex">
+                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_1.jpg);"></a>
+                            <div class="text">
+                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                                <div class="meta">
+                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
+                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-21 mb-4 d-flex">
+                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_2.jpg);"></a>
+                            <div class="text">
+                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                                <div class="meta">
+                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
+                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-21 mb-4 d-flex">
+                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_3.jpg);"></a>
+                            <div class="text">
+                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                                <div class="meta">
+                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
+                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- SIDE BAR  --}}
+
+                {{-- MAIN CONTAIN  --}}
+
                 <div class="col-md-9">
                     <div class="row mb-4">
                         <div class="col-md-12 d-flex justify-content-between align-items-center">
@@ -275,55 +342,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-3">
-                    <div class="sidebar-box ftco-animate">
-                        <div class="categories">
-                            <h3>Product Types</h3>
-                            <ul class="p-0">
-                                <li><a href="#">Brandy <span class="fa fa-chevron-right"></span></a></li>
-                                <li><a href="#">Gin <span class="fa fa-chevron-right"></span></a></li>
-                                <li><a href="#">Rum <span class="fa fa-chevron-right"></span></a></li>
-                                <li><a href="#">Tequila <span class="fa fa-chevron-right"></span></a></li>
-                                <li><a href="#">Vodka <span class="fa fa-chevron-right"></span></a></li>
-                                <li><a href="#">Whiskey <span class="fa fa-chevron-right"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="sidebar-box ftco-animate">
-                        <h3>Recent Blog</h3>
-                        <div class="block-21 mb-4 d-flex">
-                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_1.jpg);"></a>
-                            <div class="text">
-                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-                                <div class="meta">
-                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
-                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="block-21 mb-4 d-flex">
-                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_2.jpg);"></a>
-                            <div class="text">
-                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-                                <div class="meta">
-                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
-                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="block-21 mb-4 d-flex">
-                            <a class="blog-img mr-4" style="background-image: url(frontend/images/image_3.jpg);"></a>
-                            <div class="text">
-                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-                                <div class="meta">
-                                    <div><a href="#"><span class="fa fa-calendar"></span> Apr. 18, 2020</a></div>
-                                    <div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {{-- MAIN CONTAIN  --}}
             </div>
         </div>
     </section>
