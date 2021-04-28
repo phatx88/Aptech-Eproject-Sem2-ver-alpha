d9000675e30f586c444396eed2dcd46a96dcd147
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2021-04-29 02:28:14 +0700

    add geochart

---
+++ test/app/Http/Controllers/FetchChartDataController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Province;
+
+class FetchChartDataController extends Controller
+{
+    public function fetchOrderByProvince() {
+        $data = Province::select('type' , 'count')->get();
+        //fix vietnamese to json issues
+        echo json_encode($data,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    }
+}
--- test/routes/web.php
+++ test/routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\FetchChartDataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -128,6 +129,9 @@ Route::get('/checkout', [User_CheckOutController::class , 'index'])->name('check
 //SETUP PASSWORD FOR STAFF
 Route::get('/auth/passwordset/{token}', [PasswordSetupController::class,'passwordset']);
 
+// FETCH DATA FOR API 
+Route::get('/fetch-order-data', [FetchChartDataController::class,'fetchOrderByProvince']);
+
 //BACK END
 
 // Có VErify
@@ -179,14 +183,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::post('order/calculate-fee',[Admin_OrderController::class, 'shipping_fee']);
     Route::post('fetch/product', Admin_ProductController::class.'@fetchProduct');
 });
-
-
-// ROUTE TEST
-Route::get('/test', function () {
-    return view('auth.verify');
-});
-
-
 
 
 //URL TRẢ VỀ VIEW -> Cho development thôi
