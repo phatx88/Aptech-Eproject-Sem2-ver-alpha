99ebffff421c5ffdea3e4660ac54a7f05e71dc79
thinhlt3004 <70786576+thinhlt3004@users.noreply.github.com>
2021-05-02 14:47:10 +0700

    thinh

--- test/app/Models/Tag.php
+++ test/app/Models/Tag.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 class Tag extends Model
 {
     use HasFactory;
     protected $table = 'tag';
     public $timestamps = false;
+
+    public function tag(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class , 'post_tag' , 'postId' , 'tagId');
+    }
 }
