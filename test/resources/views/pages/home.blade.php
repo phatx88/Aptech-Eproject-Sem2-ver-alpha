c057dba16855b6ce429dfbb99a68eb6527de5950
thinhvu270499 <70786462+thinhvu270499@users.noreply.github.com>
2021-05-08 09:51:36 +0700

    blog

--- test/resources/views/pages/home.blade.php
+++ test/resources/views/pages/home.blade.php
@@ -360,74 +360,24 @@
                 </div>
             </div>
             <div class="row d-flex">
+                @foreach ($post as $post)
+
                 <div class="col-lg-6 d-flex align-items-stretch ftco-animate">
                     <div class="blog-entry d-flex">
                         <a href="blog-single.html" class="block-20 img"
-                            style="background-image: url({{ asset('frontend/images/image_1.jpg') }});">
-                        </a>
-                        <div class="text p-4 bg-light">
-                            <div class="meta">
-                                <p><span class="fa fa-calendar"></span> 23 April 2020</p>
-                            </div>
-                            <h3 class="heading mb-3"><a href="#">The Recipe from a Winemaker’s Restaurent</a></h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                            </p>
-                            <a href="#" class="btn-custom">Continue <span class="fa fa-long-arrow-right"></span></a>
-
+                        style="background-image: url({{ asset('backend/images/blogs/'. $post->featured_image) }});">
+                    </a>
+                    <div class="text p-4 bg-light">
+                        <div class="meta">
+                            <p><span class="fa fa-calendar"></span> {{ $post->publishedAt }}</p>
                         </div>
+                        <h3 class="heading mb-3"><a href="#">{{ $post->title }}</a></h3>
+                        <p>{{ $post->summary }}</p>
+                        <a href="{{ url('/blog/details/'.$post->slug) }}" class="btn-custom">Continue <span class="fa fa-long-arrow-right"></span></a>
                     </div>
                 </div>
-                <div class="col-lg-6 d-flex align-items-stretch ftco-animate">
-                    <div class="blog-entry d-flex">
-                        <a href="blog-single.html" class="block-20 img"
-                            style="background-image: url({{ asset('frontend/images/image_2.jpg') }});">
-                        </a>
-                        <div class="text p-4 bg-light">
-                            <div class="meta">
-                                <p><span class="fa fa-calendar"></span> 23 April 2020</p>
-                            </div>
-                            <h3 class="heading mb-3"><a href="#">The Recipe from a Winemaker’s Restaurent</a></h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                            </p>
-                            <a href="#" class="btn-custom">Continue <span class="fa fa-long-arrow-right"></span></a>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 d-flex align-items-stretch ftco-animate">
-                    <div class="blog-entry d-flex">
-                        <a href="blog-single.html" class="block-20 img"
-                            style="background-image: url({{ asset('frontend/images/image_3.jpg') }});">
-                        </a>
-                        <div class="text p-4 bg-light">
-                            <div class="meta">
-                                <p><span class="fa fa-calendar"></span> 23 April 2020</p>
-                            </div>
-                            <h3 class="heading mb-3"><a href="#">The Recipe from a Winemaker’s Restaurent</a></h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                            </p>
-                            <a href="#" class="btn-custom">Continue <span class="fa fa-long-arrow-right"></span></a>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 d-flex align-items-stretch ftco-animate">
-                    <div class="blog-entry d-flex">
-                        <a href="blog-single.html" class="block-20 img"
-                            style="background-image: url({{ asset('frontend/images/image_4.jpg') }});">
-                        </a>
-                        <div class="text p-4 bg-light">
-                            <div class="meta">
-                                <p><span class="fa fa-calendar"></span> 23 April 2020</p>
-                            </div>
-                            <h3 class="heading mb-3"><a href="#">The Recipe from a Winemaker’s Restaurent</a></h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                            </p>
-                            <a href="#" class="btn-custom">Continue <span class="fa fa-long-arrow-right"></span></a>
-
-                        </div>
-                    </div>
-                </div>
+            </div>
+            @endforeach
             </div>
         </div>
     </section>
